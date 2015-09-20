4eb14c5e051a70ca85b77f6b31b1a719a1a6c6ed
nadjibamar <hello@ebabel.eu>
2015-09-20 13:12:26 +0200

    added grunt and documentation generator JSDoc

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        globals: {
+          require: true,
+          module: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['src/**/*.js']
+      }
+    },
+    jsdoc: {
+      dist : {
+        src: ['src/**/*.js'],
+        options: {
+          destination: 'doc'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+  // Default task.
+  grunt.registerTask('default', [
+    'jshint',
+    'jsdoc'
+  ]);
+
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 var starWarsNames = require('./starwars-names.json');
 var uniqueRandomArray = require('unique-random-array');
 
+/**
+ * Main random Star Wars names selector.
+ * @type module
+ */
 module.exports = {
+    /**
+     * Array of all the Star Wars names.
+     * @type array
+     */
     all: starWarsNames,
+
+    /**
+     * Get a single Star Wars name, selected randomly.
+     * @type function
+     */
     random: uniqueRandomArray(starWarsNames)
 };
