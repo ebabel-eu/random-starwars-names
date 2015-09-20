46d06f7bf4f44ba6f982b7598eceda24bc027731
nadjibamar <hello@ebabel.eu>
2015-09-20 15:15:40 +0200

    first unit test

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var startWars = require('./index');
+
+describe('random-starwars-names', function() {
+    it('should work!', function() {
+        expect(true).to.be.true;
+    });
+});
